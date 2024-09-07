26383870ec3d6915d5899078e06d69a0f52f5277
FeHunter <hunterfe1111@gmail.com>
2024-09-07 18:12:48 -0300

    Criado Contexto

--- Programa2_TP3CSharp_ASP.NET/Data/Contexto.cs
+++ Programa2_TP3CSharp_ASP.NET/Data/Contexto.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Programa2_TP3CShpar_ASP.NET.Models;
 
 namespace Programa2_TP3CSharp_ASP.NET.Data
 {
     public class Contexto : DbContext
     {
+        public DbSet<Pessoa> Pessoas { get; set; }
+        public DbSet<Passaporte> Passaportes { get; set; }
 
+        public Contexto(DbContextOptions<Contexto> options) : base(options) {}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Programa2_TP3CSharp_ASP.NET/Program.cs
+++ Programa2_TP3CSharp_ASP.NET/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Programa2_TP3CSharp_ASP.NET.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Contexto>(options => options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
